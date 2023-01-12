93456d7f0b5dbea540d598f790cffb0a2bf3e540
Peter Gyenge <68734703+petergyenge@users.noreply.github.com>
2023-01-12 14:41:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ document.getElementById("eredmeny").innerHTML = result*/
 //Eredmény kiírása html oldalra kész.
 
 function megjelenites(){
-var text = document.getElementById("szoveg").value;
-var toChange = document.getElementById("mit").value;
-var newWord = document.getElementById("mire").value;
+	var text = document.getElementById("szoveg").value;
+	var toChange = document.getElementById("mit").value;
+	var newWord = document.getElementById("mire").value;
 
-var result = censor(text, toChange, newWord);
-document.getElementById("eredmeny").innerHTML = result
+	var result = censor(text, toChange, newWord);
+	document.getElementById("eredmeny").innerHTML = result
 return false;
 }
 
